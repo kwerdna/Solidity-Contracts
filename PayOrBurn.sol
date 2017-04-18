d102449707a939f9946fb60ce3dddca918b16cec
andy k <andy@eborrower.com>
2017-04-17 20:05:41 -0700

    new version allows dialog between buyer and actor

--- PayOrBurn.sol
+++ PayOrBurn.sol
@@ -1,46 +1,87 @@
 pragma solidity 0.4.8;
 contract payOrBurn {
-	address buyer;
-	address actor;
-	string buyerRequest;
-	string actorData;
-	enum State {Requested, ActedOn}
-	State state;
-	modifier inState (State s) {if (state != s) throw; _;}
-	modifier condition(bool c) {if (!c) throw; _;  }
-	modifier onlyBuyer() { if (msg.sender != buyer) throw; _;}
-	event RequestPosted (string request);
-	event ActorReplied (string answer);
-	event ActorPaid(uint amount);
-	event FundsBurned(uint amount);
-	function payOrBurn(string _BuyerRequest) payable {
-		buyer = msg.sender;
-		buyerRequest = _BuyerRequest;
-		RequestPosted(_BuyerRequest);
-	}
-	function actOnRequest(string _ActorData) inState(State.Requested) {
-		actor = msg.sender;
-		actorData = _ActorData;
-		ActorReplied(_ActorData);
-		state = State.ActedOn;
-	}
-	function payActor(uint _amount)
-		onlyBuyer
-		inState(State.ActedOn)
-		condition(_amount <= this.balance)
-	{
-		if (!actor.send(_amount)) throw;
-		ActorPaid(_amount);
-	}
-	function burnFunds(uint _amount)
-		onlyBuyer
-		inState(State.ActedOn)
-		condition(_amount <= this.balance)
-	{
-		if (!burnAddress().send(_amount)) throw;
-		FundsBurned(_amount);
-	}
-	function burnAddress() constant returns(address) {
-		return 0xdead;   
-	} 
+    address buyer;
+    address actor;
+    address constant burnAddress = 0xdead;
+    string buyerRequest;
+    uint deposit;
+    string actorData;
+    enum State {Requested, Committed}
+    State state;
+
+    modifier inState (State s) {if (state != s) throw; _;}
+    modifier condition(bool c) {if (!c) throw; _;  }
+    modifier onlyBuyer() { if (msg.sender != buyer) throw; _;}
+    modifier onlyActor() { if (msg.sender != actor) throw; _;}
+
+    event RequestPosted (address requestedBy, string request, uint value, uint deposit);
+    event RequestUpdated (string request);
+    event ActorCommitted (address _actor, string answer, uint _depositAmount);
+    event ActorDataUpdated (string answer);
+    event ActorPaid(uint amount);
+    event FundsBurned(uint amount);
+    event RequestCanceled(uint amount);
+
+    function () payable {}
+
+    function payOrBurn(string _BuyerRequest, uint _depositInWei) payable {
+        buyer = msg.sender;
+        deposit = _depositInWei;
+        buyerRequest = _BuyerRequest;
+        RequestPosted(buyer, buyerRequest, msg.value, deposit);
+    }
+
+    function updateBuyerRequest(string _BuyerRequest)
+        onlyBuyer
+        condition(this.balance > 0)
+    {
+        buyerRequest = _BuyerRequest;
+        RequestUpdated(buyerRequest);
+    }
+
+    function cancelRequest()
+        onlyBuyer
+        inState(State.Requested)
+    {
+        RequestCanceled(this.balance);
+        selfdestruct(buyer);
+    }
+
+    function commitToFulfillRequest(string _ActorData)
+        payable
+        inState(State.Requested)
+        condition(msg.value == deposit)
+    {
+        actor = msg.sender;
+        actorData = _ActorData;
+        ActorCommitted(actor, actorData, msg.value);   
+        state = State.Committed;
+    }
+
+    function updateActorData( string _ActorData)
+        inState(State.Committed)
+        onlyActor
+        condition(this.balance > 0)
+    {
+        actorData = _ActorData;
+        ActorDataUpdated(actorData);
+    }
+
+    function payActor(uint _amount)
+        onlyBuyer
+        inState(State.Committed)
+        condition(_amount <= this.balance)
+    {
+        if (!actor.send(_amount)) throw;
+        ActorPaid(_amount);
+    }
+
+    function burnFunds(uint _amount)
+        onlyBuyer
+        inState(State.Committed)
+        condition(_amount <= this.balance)
+    {
+        if (!burnAddress.send(_amount)) throw;
+        FundsBurned(_amount);
+    }
 }
