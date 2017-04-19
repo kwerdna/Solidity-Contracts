8effb74ecf113ae12ca8efb3c6be3a80fd0e5ab1
andy k <andy@eborrower.com>
2017-04-18 21:40:11 -0700

    crowd funded BOP

---
+++ GoFundOrBurnIt.sol
@@ -0,0 +1,75 @@
+pragma solidity 0.4.8;
+// crowdfunded BOP (Burnable Open Payment)
+// the 'artist' has an idea for a project but needs funding
+// they create a contract which includes a message.
+// min pledge amd min payment/burn amounts are optional
+// after funders pledge eth they can only pay the artist or burn the eth
+// each funder controls the amount of eth they have pledged
+// the artist and funder(s) can communicate thru event/messages as the project progresses
+// funders can pledge more funds at later stages in the project
+
+contract goFundOrBurnIt {
+    address artist = msg.sender;
+    address constant burnAddress = 0xdead;
+    uint minimumPledge;
+    uint minimumPayment;
+    mapping (address => uint) funder;
+ 
+    modifier condition(bool c) {if (!c) throw; _;  }
+    modifier onlyArtist() { if (msg.sender != artist) throw; _;}
+    modifier onlyFunder() { if (funder[msg.sender] <= 0) throw; _;}
+
+    event RequestMade(address Artist, string Message, uint MinPledge, uint MinPayment);
+    event FundsPledged(address Funder, string Message, uint Amount, uint FundersBalance);
+    event ArtistSentMessage(string Message);
+    event FunderSentMessage(address funder, string Message, uint FundersBalance);
+    event ArtistPaid(address Funder, uint Amount, string Message, uint FundersBalance);
+    event FundsBurned(address Funder, uint Amount, string Message, uint FundersBalance);
+
+    function goFundOrBurnIt(string Message, uint minPledge, uint minPayment) 
+        condition (minPledge >= minPayment)
+    {
+        minimumPledge = minPledge;
+        minimumPayment = minPayment;
+        RequestMade(msg.sender, Message, minPledge, minPayment);
+    }
+
+    function artistSendMessage(string Message) onlyArtist {
+        ArtistSentMessage(Message);
+    }
+
+    function pledgeFunds(string Message)
+        payable
+        condition(msg.sender != artist)
+        condition(msg.value >= minimumPledge)
+    {
+        funder[msg.sender] += msg.value;
+        FundsPledged(msg.sender, Message, msg.value, funder[msg.sender]);
+    }
+
+    function funderSendMessage(string Message)
+        onlyFunder
+    {
+        FunderSentMessage(msg.sender, Message, funder[msg.sender]);
+    }
+
+    function payArtist(uint _amount, string Message)
+        onlyFunder
+        condition(funder[msg.sender] >= _amount)
+        condition(_amount >= minimumPayment)
+    {
+        funder[msg.sender] -= _amount;
+        if (!artist.send(_amount)) throw;
+        ArtistPaid(msg.sender, _amount, Message, funder[msg.sender]);
+    }
+
+    function burnFunds(uint _amount, string Message)
+        onlyFunder
+        condition(funder[msg.sender] >= _amount)
+        condition(_amount >= minimumPayment)
+    {
+        funder[msg.sender] -= _amount;
+        if (!burnAddress.send(_amount)) throw;
+        FundsBurned(msg.sender, _amount, Message, funder[msg.sender]);
+    }
+}
